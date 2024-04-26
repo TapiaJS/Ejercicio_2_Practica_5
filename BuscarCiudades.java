3a02c354ae8a9e20c71936330c654fb683c58687
TapiaJS <m03tapiajs@gmail.com>
2024-04-25 21:48:52 -0600

    se separa código de la clase main a la clase BuscarCiudades

---
+++ BuscarCiudades.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class BuscarCiudades {
+
+    public static void leerArchivo(List<Ciudad> ciudades, String fileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+
+            long contador = Files.lines(Paths.get(fileName)).count();
+            System.out.println("READ " + fileName + ": " + contador);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 4) {
+                    ciudades.add(new Ciudad(parts[0], parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3])));
+                    Colors.println(Colors.HIGH_INTENSITY + ciudades.size() + ". " + line, Colors.CYAN);
+                } else {
+                    Colors.println(Colors.HIGH_INTENSITY + "Formato de línea incorrecto: " + line, Colors.RED);
+                }
+            }
+        }
+    }
+
+    public static int getInt(String mensaje, String error, int min, int max) {
+        int val;
+        Scanner m = new Scanner(System.in);
+        while (true) {
+            Colors.println(mensaje, Colors.HIGH_INTENSITY);
+            if (m.hasNextInt()) {
+                val = m.nextInt();
+                if (val < min || max < val) {
+                    Colors.println(error, Colors.RED + Colors.HIGH_INTENSITY);
+                } else {
+                    return val;
+                }
+            } else {
+                m.nextLine();
+                Colors.println(error, Colors.RED + Colors.HIGH_INTENSITY);
+            }
+        }
+    }
+
+    public static void obtenerCoordenadas(List<Ciudad> ciudades, Scanner m) {
+        String error = Colors.HIGH_INTENSITY + "Ingresa una opción válida";
+        String mensaje = "";
+        int coordenadaXMIN = 0;
+        int coordenadaXMAX = 0;
+        int coordenadaYMIN = 0;
+        int coordenadaYMAX = 0;
+        int valorAuxiliarX = 0;
+        int valorAuxiliarY = 0;
+
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    Ciudad.setMaxX(Ciudad.getMaxX() - 1);
+                    mensaje = "¿Cuál es la primera coordenada X?" + Ciudad.mostrarMinMaxX();
+
+                    coordenadaXMIN = getInt(mensaje, error, Ciudad.getMinX(), Ciudad.getMaxX());
+
+                    valorAuxiliarX = Ciudad.getMinX();
+                    Ciudad.setMinX(coordenadaXMIN);
+                    break;
+                case 1:
+                    Ciudad.setMaxX(Ciudad.getMaxX() + 1);
+                    mensaje = "¿Cuál es la segunda coordenada X?" + Ciudad.mostrarMinMaxX();
+
+                    coordenadaXMAX = getInt(mensaje, error, Ciudad.getMinX(), Ciudad.getMaxX());
+
+                    Ciudad.setMinX(valorAuxiliarX);
+                    break;
+                case 2:
+                    Ciudad.setMaxY(Ciudad.getMaxY() - 1);
+                    mensaje = "¿Cuál es la primera coordenada Y?" + Ciudad.mostrarMinMaxY();
+
+                    coordenadaYMIN = getInt(mensaje, error, Ciudad.getMinY(), Ciudad.getMaxY());
+
+                    valorAuxiliarY = Ciudad.getMinY();
+                    Ciudad.setMinY(coordenadaYMIN);
+                    break;
+                case 3:
+                    Ciudad.setMaxY(Ciudad.getMaxY() + 1);
+                    mensaje = "¿Cuál es la segunda coordenada Y?" + Ciudad.mostrarMinMaxY();
+
+                    coordenadaYMAX = getInt(mensaje, error, Ciudad.getMinY(), Ciudad.getMaxY());
+
+                    Ciudad.setMinX(valorAuxiliarY);
+                    break;
+            }
+        }
+        Ciudad.encontrarCoincidencias(ciudades, coordenadaXMIN, coordenadaXMAX, coordenadaYMIN, coordenadaYMAX);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,153 +1,21 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner m = new Scanner(System.in);
-
         List<Ciudad> ciudades = new ArrayList<>();
-        String fileName ="ciudades.txt";
-
-        /*
-        * try-with-resources garantiza que cada recurso se cierre al final de la declaración
-        */
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) { 
-
-            /*
-             * lee todas las líneas del archivo y luego cuenta el número total de líneas en el archivo
-             */
-            long contador = Files.lines(Paths.get(fileName)).count();
-            System.out.println("READ " + fileName + ": " + contador);
-            
-            /*
-             * se inicializa una cadena line con las líneas leídas del archivo
-             */
-            String line;
-            while ((line = reader.readLine()) != null) {
-                /*
-                 * mientras haya una línea disponible en el archivo. Se divide la línea leída en partes utilizando el espacio como delimitador
-                 */
-                String[] parts = line.split(" ");
-                /*
-                 * si la línea tiene exactamente 4 partes. Se asume que la línea contiene información válida para crear un objeto tipo Ciudad 
-                 */
-                if (parts.length == 4) {
-                    ciudades.add(new Ciudad(parts[0], parts[1], Integer.parseInt(parts[2]), Integer.parseInt(parts[3])));
-                    Colors.println(Colors.HIGH_INTENSITY + ciudades.size() + ". " + line, Colors.CYAN);
-                } else {
-                    Colors.println(Colors.HIGH_INTENSITY + "Formato de línea incorrecto: " + line, Colors.RED);
-                }
-            }
-
+        String fileName = "ciudades.txt";
+        
+        try {
+            BuscarCiudades.leerArchivo(ciudades, fileName);
         } catch (IOException e) {
             System.out.println("Error al leer el archivo: " + e.getMessage());
-        } catch (NumberFormatException e) {
-            System.out.println("Error al convertir coordenadas a números: " + e.getMessage());
         }
-
+        
         Ciudad.encontrarMaxMin(ciudades);
-        String error = Colors.HIGH_INTENSITY + "Ingresa una opción válida";
-        boolean datoErroneo = false;
-        int coordenadaXMIN = 0;
-        int coordenadaXMAX = 0;
-        int coordenadaYMIN = 0;
-        int coordenadaYMAX = 0;
-        int valorAuxiliarX = 0;
-        int valorAuxiliarY = 0;
-
-        for (int i = 0; i < 4; i++) {
-            switch (i) {
-                case 0:
-                Ciudad.setMaxX(Ciudad.getMaxX() - 1);
-                    do {
-                        Colors.println("¿Cuál es la primera coordenada X?" + Ciudad.mostrarMinMaxX(), Colors.HIGH_INTENSITY);
-                        try {
-                            coordenadaXMIN = m.nextInt();
-                            if (!Ciudad.validarCordenada(coordenadaXMIN, 0)) {
-                                Colors.println(error, Colors.RED);
-                                datoErroneo = true;
-                            } else {
-                                datoErroneo = false;
-                                valorAuxiliarX = Ciudad.getMinX();
-                                Ciudad.setMinX(coordenadaXMIN);
-                            }
-                        } catch (InputMismatchException e) {
-                            Colors.println(error, Colors.RED);
-                            m.nextLine(); // Limpiar el buffer
-                            datoErroneo = true;
-                        }
-                    } while (datoErroneo);
-                    break;
-                case 1:
-                Ciudad.setMaxX(Ciudad.getMaxX() + 1);
-                    do {
-                        Colors.println("¿Cuál es la segunda coordenada X?" + Ciudad.mostrarMinMaxX(), Colors.HIGH_INTENSITY);
-                        try {
-                            coordenadaXMAX = m.nextInt();
-                            if (!Ciudad.validarCordenada(coordenadaXMAX, 0)) {
-                                Colors.println(error, Colors.RED);
-                                datoErroneo = true;
-                            } else {
-                                datoErroneo = false;
-                                Ciudad.setMinX(valorAuxiliarX);
-                            }
-                        } catch (InputMismatchException e) {
-                            Colors.println(error, Colors.RED);
-                            m.nextLine(); // Limpiar el buffer
-                            datoErroneo = true;
-                        }
-                    } while (datoErroneo);
-                    break;
-                case 2:
-                Ciudad.setMaxY(Ciudad.getMaxY() - 1);
-                    do {
-                        Colors.println("¿Cuál es la primera coordenada Y?" + Ciudad.mostrarMinMaxY(), Colors.HIGH_INTENSITY);
-                        try {//Utilizar getInt
-                            coordenadaYMIN = m.nextInt();
-                            if (!Ciudad.validarCordenada(coordenadaYMIN, 1)) {
-                                Colors.println(error, Colors.RED);
-                                datoErroneo = true;
-                            } else {
-                                datoErroneo = false;
-                                valorAuxiliarY = Ciudad.getMinY();
-                                Ciudad.setMinY(coordenadaYMIN);
-                            }
-                        } catch (InputMismatchException e) {
-                            Colors.println(error, Colors.RED);
-                            m.nextLine(); // Limpiar el buffer
-                            datoErroneo = true;
-                        }
-                    } while (datoErroneo);
-                    break;
-                case 3:
-                Ciudad.setMaxY(Ciudad.getMaxY() + 1);
-                    do {
-                        Colors.println("¿Cuál es la segunda coordenada Y?" + Ciudad.mostrarMinMaxY(), Colors.HIGH_INTENSITY);
-                        try {
-                            coordenadaYMAX = m.nextInt();
-                            if (!Ciudad.validarCordenada(coordenadaYMAX, 1)) {
-                                Colors.println(error, Colors.RED);
-                                datoErroneo = true;
-                            } else {
-                                datoErroneo = false;
-                                Ciudad.setMinY(valorAuxiliarY);
-                            }
-                        } catch (InputMismatchException e) {
-                            Colors.println(error, Colors.RED);
-                            m.nextLine(); // Limpiar el buffer
-                            datoErroneo = true;
-                        }
-                    } while (datoErroneo);
-                    break;
-            }
-        }
-        Ciudad.encontrarCoincidencias(ciudades, coordenadaXMIN, coordenadaXMAX, coordenadaYMIN, coordenadaYMAX);
+        BuscarCiudades.obtenerCoordenadas(ciudades, m);
     }
 }
